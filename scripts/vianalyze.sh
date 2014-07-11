0ec3298fe468a8c7af0a202d1b221e353d510150
Rosa Anneke Luirink <rosaluirink@gmail.com>
2014-07-11 09:46:47 -0700

    Added scripts directory

---
+++ scripts/vianalyze.sh
@@ -0,0 +1,8 @@
+#!/bin/tcsh
+
+#setenv YANKHOME ${HOME}/yank/yank.choderalab
+setenv YANKHOME ${HOME}/anaconda/lib/python2.7/site-packages
+
+# Run in serial mode.
+#python ${YANKHOME}/yank/analyze.py
+vi ${YANKHOME}/yank/analyze.py
